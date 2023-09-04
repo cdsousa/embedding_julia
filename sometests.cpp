4dfff33839997e1f9f763424a991ab70bba0bcf5
Cristóvão D. Sousa <crisjss@gmail.com>
2023-09-04 11:39:27 +0100

    Try jl_adopt_thread

--- sometests.cpp
+++ sometests.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <thread>
+#include <atomic>
 #include <chrono>
 
-
-
-#define JULIA_ENABLE_THREADING
-#include <platform.h>
+// #define JULIA_ENABLE_THREADING
+// #include <platform.h>
 #include <julia.h>
 
-extern "C"{
-    void jl_init_threadtls(int16_t tid);
-}
+
+std::atomic<bool> done(false);
 
 
 void tf(int (*f)(int)){
     std::cout << ">>>>" << std::endl;
 
+    jl_adopt_thread();
+
     jl_threadid();
 
-    // jl_eval_string("println(20)");
-    // std::cout << f(123) <<std::endl;
+    jl_eval_string("println(20)");
+    std::cout << f(123) <<std::endl;
 
     std::cout << "<<<<" << std::endl;
+    done = true;
 }
 
 
@@ -31,24 +32,30 @@ int main(int argc, char* argv[]) {
 
     std::cout << "---------------------" << std::endl;
 
-    jl_init(); 
+    jl_init();
     jl_eval_string("println(10)");
 
-    jl_eval_string("f(x::Int)::Int = 2x");
+    jl_eval_string("f(x::Int)::Int = (println(1111); 2x)");
     int (*f)(int) = (int (*)(int))(jl_unbox_voidpointer(jl_eval_string("@cfunction(f, Int, (Int,))")));
 
-    // size_t __excstack_state = jl_excstack_state(); 
+    // size_t __excstack_state = jl_excstack_state();
     // f(123);
 
     std::thread t(tf, f);
-    t.join();
+    std::cout << "--- 1" << std::endl;
+    std::cout << "--- 2" << std::endl;
 
     jl_eval_string("println(Threads.nthreads())");
 
-    jl_eval_string("a=zeros(4); Threads.@threads for i=1:4; a[i]=Threads.threadid(); end; println(a)"); 
-
     jl_eval_string("println(11)");
-    jl_atexit_hook(0); 
+
+    while(!done){
+        jl_eval_string("sleep(0.001)");
+    }
+    std::cout << "join..." << std::endl;
+    t.join();
+
+    jl_atexit_hook(0);
 
     return 0;
 }
